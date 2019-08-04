ee882dd715debc4fc03c44617bf16aead380e5ed
Brendan Richards <brendan_richards_msdn@hotmail.com>
2019-08-04 22:19:07 +1000

    obsolete file

--- Northwind.EF.Application.Performance/TestCustomerGenerator.cs
+++ Northwind.EF.Application.Performance/TestCustomerGenerator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using Bogus;
-using Northwind.EF.Application.Performance.Fakes;
 using Northwind.EF.Domain.Entities;
 
 namespace Northwind.EF.Application.Performance
